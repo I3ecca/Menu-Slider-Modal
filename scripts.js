a20d3d9529c84008f4889e2e0343dd9750655f0a
RLant <rebeccalant@gmail.com>
2021-03-03 18:20:31 -0500

    finishing CSS and adding JS and fixing HTML typo

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const toggle = document.getElementById("toggle")
+const close = document.getElementById("close")
+const open = document.getElementById("open")
+const modal = document.getElementById("modal")
+
+//toggle nav
+
+toggle.addEventListener("click", function(){
+    document.body.classList.toggle('show-nav')
+});
+
+//show modal
+open.addEventListener("click", function(){
+    modal.classList.add("show-modal");
+})
+
+//close button, hide modal
+close.addEventListener("click", function(){
+    modal.classList.remove("show-modal");
+})
+
+// hide modal on outside click
+window.addEventListener("click", function(e){
+    e.target === modal ? modal.classList.remove("show-modal") : false
+})
